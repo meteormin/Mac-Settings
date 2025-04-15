1136fed5307c88782432b111fcf18d01883194e7
meteormin <miniyu97@gmail.com>
2025-04-15 14:16:05 +0900

    fix: install brew shell script

--- devtools/bin/install-brew.sh
+++ devtools/bin/install-brew.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-PACKAGES="packages.txt"
-
-if ! command -v brew &> /dev/null;then
+# Check for Homebrew
+if ! command -v brew &> /dev/null; then
   echo "Homebrew is not installed."
   echo "Installing Homebrew..."
-  install_brew=$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
-  echo $install_brew
 
-  echo "Install Homebrew successfully."
-  echo "Try install cask"
-  brew install cask
-  echo "Install cask successfully."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo "✅ Homebrew installed successfully."
 else
-  echo "Homebrew is already installed."
+  echo "✅ Homebrew is already installed."
 fi
 
-exit 0;
